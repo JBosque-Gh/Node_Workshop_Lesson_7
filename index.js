a60095129363c1983467aee38bce6464586ab587
JBosque-Gh <javibosque@hotmail.com>
2026-01-04 11:29:06 +0000

    fix index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const n1 = Number(core.getInput('number1') || 0);
-  const n2 = Number(core.getInput('number2') || 0);
+  const jsNum1 = Number(core.getInput('number1') || 0);
+  const jsNum2 = Number(core.getInput('number2') || 0);
   result = parseInt(jsNum1) + parseInt(jsNum2);
   core.setOutput('reult', result);
 } catch (error) {
